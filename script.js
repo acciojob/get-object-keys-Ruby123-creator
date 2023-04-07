339cc764fed967c6eea0671b858a2c27a1dbf8e8
Ruby pal <rubyp20001@gmail.com>
2023-04-07 19:02:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+//your JS code here. If required.
+// create student object with name property
+const student = {
+  name: "John"
+};
+
+// add getKeys method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+}
+
+// test the getKeys method on the student object
+console.log(student.getKeys()); // output: ["name"]
